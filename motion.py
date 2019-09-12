e2649fce4c6901a00eefc850c69982ec3633d7c3
Adarsh Padhi <45957713+adarsh1405@users.noreply.github.com>
2019-09-12 23:06:27 +0530

    Added with a bug 2

--- motion.py
+++ motion.py
@@ -15,39 +15,29 @@ def face_detect():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 #face_detect()
-
 def video():
-    first_frame=None
     video = cv2.VideoCapture(0)
-    while(True):
-        check, frame = video.read()
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray=cv2.GaussianBlur(gray,(21,21),0)
-
-        if first_frame is None:
-            first_frame=gray
-            continue
-
-
-        delta_frame=cv2.absdiff(first_frame,gray)
-        threshold=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
-        threshold=cv2.dilate(threshold,None,iterations=2)
-
-        (_,cnts,_)=cv2.findContours(threshold.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        for contour in cnts:
-            if cv2.contourArea(contour) < 10000:
-                continue
-            (x, y, w, h)=cv2.boundRect(contour)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
-
-
-        cv2.imshow("Threshold Frame",threshold)
-        cv2.imshow("capture",gray)
-        print(gray)
-        key=cv2.waitKey(1)
-        if key== ord('c'):
-            break
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    while True:
+    	ret,frame = video.read()
+    	gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    	if ret == False:
+    		continue
+    	faces = face_cascade.detectMultiScale(gray_frame,1.3,5)
+    	if len(faces) == 0:
+    		continue
+    	for face in faces:
+    		x,y,w,h = face
+    		offset = 10
+    		face_offset = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+    		face_selection = cv2.resize(face_offset,(100,100))
+    		cv2.imshow("Face", face_selection)
+    		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    	cv2.imshow("faces",frame)
+    	key_pressed = cv2.waitKey(1) & 0xFF
+    	if key_pressed == ord('q'):
+    		break
     video.release()
     cv2.destroyAllWindows()
+
 video()
