d300b30ec3fef4eb8ac713c6778644fbc2ba9a11
Adarsh Padhi <45957713+adarsh1405@users.noreply.github.com>
2019-09-16 22:20:27 +0530

    issue resolved and detection working now

--- motion.py
+++ motion.py
@@ -1,6 +1,5 @@
-import numpy as np
-import cv2
-
+import numpy
+import cv2,time
 def face_detect():
     face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     img=cv2.imread("photo.jpg")
@@ -15,29 +14,43 @@ def face_detect():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 #face_detect()
-def video():
-    video = cv2.VideoCapture(0)
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
-    while True:
-    	ret,frame = video.read()
-    	gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    	if ret == False:
-    		continue
-    	faces = face_cascade.detectMultiScale(gray_frame,1.3,5)
-    	if len(faces) == 0:
-    		continue
-    	for face in faces:
-    		x,y,w,h = face
-    		offset = 10
-    		face_offset = frame[y-offset:y+h+offset,x-offset:x+w+offset]
-    		face_selection = cv2.resize(face_offset,(100,100))
-    		cv2.imshow("Face", face_selection)
-    		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-    	cv2.imshow("faces",frame)
-    	key_pressed = cv2.waitKey(1) & 0xFF
-    	if key_pressed == ord('q'):
-    		break
-    video.release()
-    cv2.destroyAllWindows()
+def video_detect():
+    first_frame=None
+    video=cv2.VideoCapture(0)
 
-video()
+    while True:
+        check,frame=video.read()
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        gray=cv2.GaussianBlur(gray,(21,21),0)
+
+
+        if first_frame is None:
+            first_frame=gray
+            continue
+        delta_frame=cv2.absdiff(first_frame,gray)
+        thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
+        thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
+
+        (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in cnts:
+            if cv2.contourArea(contour) < 1000:
+                continue
+            (x,y,w,h)=cv2.boundingRect(contour)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+        """cv2.imshow("capturing",gray)
+        cv2.imshow("delta",delta_frame)
+        cv2.imshow("Thresh",thresh_frame)"""
+        cv2.imshow("color frame",frame)
+        key=cv2.waitKey(1)
+        print(gray)
+        if key==ord('c'):
+            break
+
+
+    video.release()
+    cv2.destroyAllWindows
+    print("hello")
+
+
+video_detect()
