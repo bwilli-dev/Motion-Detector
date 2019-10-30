813d6eb61760943da1c97c2ccaec6e17319a7fc8
TheSpeedX <ggspeedx29@gmail.com>
2019-10-30 19:47:26 +0530

    Added Excepion Handle To remove video corruption

--- motion.py
+++ motion.py
@@ -13,47 +13,48 @@ def face_detect():
     cv2.imshow('Gray',resized)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-#face_detect()
 def video_detect():
     first_frame=None
     video=cv2.VideoCapture(0)
     out = cv2.VideoWriter('output.mp4', -1, 20.0, (640,480))
-
-    while True:
-        check,frame=video.read()
-        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        gray=cv2.GaussianBlur(gray,(21,21),0)
-        #if check==True:
-            #frame = cv2.flip(frame,0)
-            #out.write(frame)
-        if first_frame is None:
-            first_frame=gray
-            continue
-        delta_frame=cv2.absdiff(first_frame,gray)
-        thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
-        thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
-
-        (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-        for contour in cnts:
-            if cv2.contourArea(contour) < 1000:
+    try:
+        while True:
+            check,frame=video.read()
+            gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            gray=cv2.GaussianBlur(gray,(21,21),0)
+            #if check==True:
+                #frame = cv2.flip(frame,0)
+                #out.write(frame)
+            if first_frame is None:
+                first_frame=gray
                 continue
-            (x,y,w,h)=cv2.boundingRect(contour)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-            frame = cv2.flip(frame,0)
-            frame = cv2.flip(frame,0)
-            out.write(frame)
-        """cv2.imshow("capturing",gray)
-        cv2.imshow("delta",delta_frame)
-        cv2.imshow("Thresh",thresh_frame)"""
-        cv2.imshow("color frame",frame)
-        key=cv2.waitKey(1)
-        print(gray)
-        if key==ord('c'):
-            break
-    video.release()
-    cv2.destroyAllWindows
-    print("hello")
+            delta_frame=cv2.absdiff(first_frame,gray)
+            thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
+            thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
 
+            (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
+            for contour in cnts:
+                if cv2.contourArea(contour) < 1000:
+                    continue
+                (x,y,w,h)=cv2.boundingRect(contour)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                frame = cv2.flip(frame,0)
+                frame = cv2.flip(frame,0)
+                out.write(frame)
+            """cv2.imshow("capturing",gray)
+            cv2.imshow("delta",delta_frame)
+            cv2.imshow("Thresh",thresh_frame)"""
+            cv2.imshow("color frame",frame)
+            key=cv2.waitKey(1)
+            print(gray)
+            if key==ord('c'):
+                break
+    except:
+	    print("Exception Occurred!!!")
+	finally:
+        video.release()
+        cv2.destroyAllWindows()
+
+#facedetect()
 video_detect()
